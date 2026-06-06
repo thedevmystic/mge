0cbcdbe10e23d96e5d1638507739e14b1f4910e1
Surya! <thedevmystic@gmail.com>
2026-06-06 15:59:23 +0530

    feat: add third party iwyu
    * feat: add iwyu
    
    * feat: add support to optionally pin point versions
    
    * feat: change cmake minimum version to 3.25

--- cmake/mge_options.cmake
+++ cmake/mge_options.cmake
@@ -6,4 +6,7 @@ option(MGE_ENABLE_DOCS     "Build documentation." ON)
 option(MGE_ENABLE_TESTING  "Build test suites."   ON)
 
 # Third Party Libraries Options
+# Catch2
 option(MGE_USE_SYSTEM_CATCH2 "Use system-installed Catch2 v3." OFF)
+# IWYU
+option(MGE_USE_IWYU "Use include-what-you-use (only supports system-installed)." OFF)
--- cmake/mge_third_party_config.cmake
+++ cmake/mge_third_party_config.cmake
@@ -3,15 +3,28 @@
 # LIB_NAME: Normal name of the library, e.g., Catch2
 # GIT_TAG: Library's Git Tag.
 # GITHUB_URL: GitHub URL of the repository.
+# USE_VER_IN_FP: Whether to use version in find_package or not (ON or OFF)
 function(mge_third_party_config LIB_NAME GIVEN_TAG GITHUB_URL)
   string(TOUPPER "${LIB_NAME}" LIB_NAME_UPPER)
   string(REGEX REPLACE "^[^0-9]+" "" LIB_VER "${GIVEN_TAG}")
 
+  if(ARGV4)
+    set(USE_VER_IN_FP ${ARGV4})
+  else()
+    set(USE_VER_IN_FP ON)
+  endif()
+
   # Check for system-installed if option is set
   if(MGE_USE_SYSTEM_${LIB_NAME_UPPER})
-    message(STATUS "[MGE] - Searching for system-installed ${LIB_NAME} ${LIB_VER}...")
-    find_package(${LIB_NAME} ${LIB_VER} REQUIRED GLOBAL)
-    message(STATUS "[MGE] - System-installed ${LIB_NAME} ${LIB_VER} found.")
+    if(USE_VER_IN_FP)
+      message(STATUS "[MGE] - Searching for system-installed ${LIB_NAME} ${LIB_VER}...")
+      find_package(${LIB_NAME} ${LIB_VER} REQUIRED GLOBAL)
+      message(STATUS "[MGE] - System-installed ${LIB_NAME} ${LIB_VER} found.")
+    else()
+      message(STATUS "[MGE] - Searching for system-installed ${LIB_NAME}...")
+      find_package(${LIB_NAME} REQUIRED GLOBAL)
+      message(STATUS "[MGE] - System-installed ${LIB_NAME} found.")
+    endif()
   else()
     # Fetch from GitHub
     message(STATUS "[MGE] - Fetching ${LIB_NAME} ${LIB_VER}...")
